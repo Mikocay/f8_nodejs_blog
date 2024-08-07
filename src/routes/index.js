b85ccb59b241f4dbea550a1cfd637d176f69ab79
Mikocay <huynhtd97@gmail.com>
2024-08-07 21:07:32 +0700

    detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
-const newsRouter = require("./news");
-const siteRouter = require("./site");
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+const courseRouter = require('./courses');
 
 const route = (app) => {
-  app.use("/news", newsRouter);
+  app.use('/news', newsRouter);
 
-  app.use("/", siteRouter);
+  app.use('/', siteRouter);
+
+  app.use('/courses', courseRouter);
 };
 
 module.exports = route;
